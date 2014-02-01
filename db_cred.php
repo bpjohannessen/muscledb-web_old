40e8f45622ff925e4e6ded7ca9ca90ab645901c1
Andreas Mosti <andreas.mosti@gmail.com>
2014-02-01 15:59:48 +0100

    Update db_cred.php

--- db_cred.php
+++ db_cred.php
@@ -2,6 +2,18 @@
 /************************************************
 	Require once the real 
 	MySQL credential variable file
+	
+source file should look like this: 
+
+<?php
+/************************************************
+	The MySQL credentials file
+$dbhost = "localhost";
+$dbname = "db_muscles";
+$dbuser = "";
+$dbpass = "";
+?>
+
 ************************************************/
 require_once("../../../../db_cred.php");
 ?>
