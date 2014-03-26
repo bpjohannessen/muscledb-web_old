020b265ecea9f72ebb2a7871190606cb57dd8b64
Bjørn-Petter Johannessen <bp.johannessen@gmail.com>
2014-03-26 23:53:18 +0200

    Renaming index.html to index.php
    Had to rename index.html to index.php. I don’t want to needlessly print
    meta tag for old browsers.

--- index.php
+++ index.php
@@ -2,16 +2,28 @@
 <html>
 <head>
 	<!-- Meta -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<?php
+	#
+	# For adding meta tag for rendering in old browsers
+	# instead of printing meta tag to all browsers
+	# <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	#
+	# Source of fix: http://www.validatethis.co.uk/news/fix-bad-value-x-ua-compatible-once-and-for-all/
+	#
+	if (isset($_SERVER['HTTP_USER_AGENT']) && 
+    (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false))
+        header('X-UA-Compatible: IE=edge,chrome=1');
+    ?>	
 	<title>Muscle Search</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width">
 	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<!-- Load CSS -->
 	<link href="resources/style/style.css" rel="stylesheet" type="text/css" />
-	<!-- Load Fonts -->
+	<!-- Load Fonts from Google API -->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans:regular,bold" type="text/css" />
-	<!-- Load jQuery library -->
+	<!-- Load jQuery library from Google API -->
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<!-- Load custom js -->
 	<script type="text/javascript" src="resources/scripts/custom.js"></script>
@@ -19,7 +31,7 @@
 <body>
 	<div id="main">
 
-		<!-- Main Title -->
+		<!-- Main Title With Icon -->
 		<div class="icon"></div>
 		<h1 class="title">Muscle search</h1>
 		<h5 class="title">(searches through muscles and shows details)</h5>
@@ -33,6 +45,5 @@
 
 
 	</div>
-
 </body>
 </html>
