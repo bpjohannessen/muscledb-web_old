ffe3eb8bd871304cac4afbc7267da97f4c07236c
Bjørn-Petter Johannessen <bp@Bjrn-Petters-MacBook-Air.local>
2015-06-14 22:48:37 +0300

    New about file
    Updated file

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<!-- Load CSS -->
-	<link href="resources/style/style.css" rel="stylesheet" type="text/css" />
+	<link href="./resources/style/style.css" rel="stylesheet" type="text/css" />
 	<!-- Load Fonts from Google API -->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans:regular,bold" type="text/css" />
 	<!-- Load jQuery library from Google API -->
@@ -34,17 +34,19 @@
 		<!-- Main Title With Icon -->
 		<div class="icon"></div>
 		<h1 class="title">About</h1>
-		<p class="title">This site contains a real-time search for (mainly) skeletal muscles. It is created as a collaboration by one medical student and one computer engineer student.</p>
+        <h5 class="title"><a href="index.php">Back to search</a></h2>
+        <h5 class="title"><a href="muscle.php">Show all muscles</a></h5>
+        <h5 class="title"><a href="nerve.php">Show all nerves</a></h5>
+		<p class="title" style="padding-top: 10px;">This site contains a real-time search for (mainly) skeletal muscles. It is created as a collaboration by one medical student and one computer engineer.</p>
 		<p class="title" style="padding-top: 10px; padding-bottom: 10px; font-weight: bold;">The contributors to the project are:</p>
                 
-                <ul id="credits" class="title" style="padding-left: 30px;">
-                    <li><span class="bold">Bjørn-Petter JOHANNESSEN, stud.med.</span>, idea, lead designer, lead programmer;</li>
-                    <li><span class="bold">Andreas MOSTI, BSc</span>, programming advisor, infrastructure, Vagrant, dbDump, the one who have a good answer to all problems</li>
-                </ul>
-                
-                <h2 class="title"><a href="index.php">Back to search</a></h2>
-		<h5 class="title"><a href="muscle.php">Show all muscles</a></h5>
-		<h5 class="title"><a href="nerve.php">Show all nerves</a></h5>
+        <ul id="credits" class="title" style="padding-left: 30px;">
+            <li><span class="bold">Bjørn-Petter JOHANNESSEN, medical student</span>, idea, lead designer, lead programmer;</li>
+            <li><span class="bold">Andreas MOSTI, BSc</span>, programming advisor, infrastructure, Vagrant, dbDump, the one who have a good answer to all problems.</li>
+        </ul>
+
+        <p class="title" style="padding-top: 10px;"><span class="bold">Disclaimer:</span> This site should not, under any circumstances, be used as a source for medical knowledge. It is created as an information purpose only. If you need medical advise, please contact a medical doctor.</p>
+
 
 	</div>
 </body>
