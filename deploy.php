fcd3235cb23b480489e030f7c3aa73c3d7f4e859
Bjørn-Petter Johannessen <bp@medicus.fettenajs.com>
2017-01-24 21:53:38 +0200

    Added logging for deploy.php

--- deploy.php
+++ deploy.php
@@ -1,5 +1,39 @@
 <?php
 /**
+ * DEBUGGING BPJ
+ *
+ */
+
+// counter
+session_start();
+$counter_name = "counter.txt";
+// Check if a text file exists. If not create one and initialize it to zero.
+if (!file_exists($counter_name)) {
+    $f = fopen($counter_name, "w");
+    fwrite($f,"0");
+    fclose($f);
+}
+// Read the current value of our counter file
+$f = fopen($counter_name,"r");
+$counterVal = fread($f, filesize($counter_name));
+fclose($f);
+// Has visitor been counted in this session?
+// If not, increase counter value by one
+if(!isset($_SESSION['hasVisited'])){
+    $_SESSION['hasVisited']="yes";
+    $counterVal++;
+    $f = fopen($counter_name, "w");
+    fwrite($f, $counterVal);
+    fclose($f);
+} else {
+    echo "allerede";
+}
+
+//echo "You are visitor number $counterVal to this site";
+
+
+
+/**
  * GIT DEPLOYMENT SCRIPT
  *
  * Used for automatically deploying websites via github or bitbucket, more deets here:
@@ -16,6 +50,7 @@ $commands = array(
     'git submodule sync',
     'git submodule update',
     'git submodule status',
+    'echo `date` >> ../counter.txt'
 );
 
 // Run the commands for output
