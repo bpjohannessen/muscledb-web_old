050a28e38e459db2d87bbe4b46babaa6e614b866
Bjørn-Petter Johannessen <bpj@dips.no>
2017-01-25 14:39:47 +0100

    Added database script

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,10 @@ echo "Stopping kestrel"
 sudo systemctl stop kestrel-muscleweb.service
 exit'
 echo "Running rsync"
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/MuscleWeb/bin/debug/netcoreapp1.1/publish bp@fettenajs.com:/home/bp/www/fettenajs.com/public_html/med/muscledb-web/MuscleWeb/bin/debug/netcoreapp1.1/publish
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/MuscleWeb/bin/Debug/netcoreapp1.1/publish bp@fettenajs.com:/home/bp/www/fettenajs.com/public_html/med/muscledb-web/MuscleWeb/bin/Debug/netcoreapp1.1/publish
 ssh bp@fettenajs.com '
+cd /home/bp/www/fettenajs.com/public_html/med/muscledb-web/MuscleWeb/bin/debug/netcoreapp1.1/publish
+cat database.sql | sqlite3 muscles.db
 echo "Starting kestrel"
 sudo systemctl start kestrel-muscleweb.service
 exit'
