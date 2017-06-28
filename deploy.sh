4f47569dcda03c278e89db54b0aac903d3ffabed
Bjørn-Petter Johannessen <bp.johannessen@gmail.com>
2017-06-28 16:53:41 +0300

    Added 'debug' in deploy script #48

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 echo "SCRIPT: Nå kjører vi med openssl +++"
+echo "Openssl"
 openssl aes-256-cbc -K $encrypted_51c466c92341_key -iv $encrypted_51c466c92341_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
 eval "$(ssh-agent -s)"
+echo "chmod + ssh-add"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
+echo "pwd + cd til MuscleWeb"
+pwd
 cd MuscleWeb
+echo "dotnet restore og publish"
 dotnet restore
 dotnet publish
 echo $TRAVIS_BUILD_DIR
