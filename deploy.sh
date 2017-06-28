914b88f9a7f4fce94f95ba96c9a363fa9599920e
Bjørn-Petter Johannessen <bp.johannessen@gmail.com>
2017-06-28 17:31:06 +0300

    Edited output/'debug' in deploy.sh and removed unused commands in travis.yml. Fixed #48

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
-echo "SCRIPT: Nå kjører vi med openssl +++"
-echo "Openssl"
+echo "Executing deploy.sh"
 openssl aes-256-cbc -K $encrypted_51c466c92341_key -iv $encrypted_51c466c92341_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
 eval "$(ssh-agent -s)"
-echo "chmod + ssh-add"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
-echo "pwd + cd til MuscleWeb"
-pwd
 cd MuscleWeb
-echo "dotnet restore og publish"
+echo "Running dotnet restore"
 dotnet restore
+echo "Running dotnet publish"
 dotnet publish
 echo $TRAVIS_BUILD_DIR
+echo "Connecting to fettenajs.com with ssh"
 ssh bp@fettenajs.com '
 echo "Stopping kestrel"
 sudo systemctl stop kestrel-muscleweb.service
@@ -19,10 +17,11 @@ exit'
 echo "Running rsync"
 rm -rf /home/bp/www/fettenajs.com/public_html/med/publish
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/MuscleWeb/bin/Debug/netcoreapp1.1/publish bp@fettenajs.com:/home/bp/www/fettenajs.com/public_html/med
+echo "Connecting to fettenajs.com with (again)"
 ssh bp@fettenajs.com '
 cd /home/bp/www/fettenajs.com/public_html/med/publish
 cat database.sql | sqlite3 muscles.db
 echo "Starting kestrel"
 sudo systemctl start kestrel-muscleweb.service
 exit'
-echo "Script finished"
+echo "Ending deploy.sh"
