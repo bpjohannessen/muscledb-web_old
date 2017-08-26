349c849f37dfb5b900ced91bd756af6cc64f6276
Bjørn-Petter Johannessen <bpj@dips.no>
2017-08-26 19:09:07 +0200

    Added parts of js required for #57

--- MuscleWeb/wwwroot/js/groups.js
+++ MuscleWeb/wwwroot/js/groups.js
@@ -15,13 +15,40 @@ $(document).ready(function () {
     var $queryurl = "api/musclegroups/" + getQueryVariable('id');
 
     $.getJSON($queryurl, function (json) {
-        console.log(json);
+        
         var $ul = $("ul#grouplist");
+        var group = json;
+        renderGroup(group, $ul);
 
-        $.each(json, function (idx, muscle) {
-            $ul.append("<li><a href='muscle.html?id="+ muscle.id +"'>" + muscle.latinName + "</a></li>");
-        });   
 
+        // while(group.SubMuscleGroups.length > 0)        
+        // {
+
+        // }
+        // console.log(json);
+        // var $ul = $("ul#grouplist");
+
+        // $.each(json, function (idx, muscle) {
+        //     $ul.append("<li><a href='muscle.html?id="+ muscle.id +"'>" + muscle.latinName + "</a></li>");
+        // });   
+
+        function renderGroup(group, list){
+            console.log(list);            
+            list.append("<ul id=" + group.id + "></ul>");
+            
+            var $sublist = $("ul#" + group.id);
+
+            console.log($sublist);
+
+            $sublist.append("<li>" + group.name + "</li>");
+            $.each(group.muscles, function (idx, muscle) {
+                $sublist.append("<li><a href='muscle.html?id="+ muscle.id +"'>" + muscle.latinName + "</a></li>");
+            });  
+            $.each(group.subMuscleGroups, function(idx, subgroup){
+                renderGroup(subgroup, list);
+            });
+        }
     });
+
 
 });
